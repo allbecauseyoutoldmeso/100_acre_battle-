fb1e8f64fd94b42e190a3cfd1971c7ebe934632e
alicespyglass <alicespyglass@gmail.com>
2017-03-29 15:32:35 +0100

    implement hit points view

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -7,12 +7,17 @@ describe Battle do
       fill_in :player_2_name, with: 'Alice'
       click_button 'Submit'
       expect(page).to have_content 'Emily vs. Alice'
-      end
-
-
-
+    end
   end
 
-
+  feature 'Player hit points' do
+    scenario 'view hit points' do
+      visit('/')
+      fill_in :player_1_name, with: 'Emily'
+      fill_in :player_2_name, with: 'Alice'
+      click_button 'Submit'
+      expect(page).to have_content 'Emily: 10HP'
+    end
+  end
 
 end
