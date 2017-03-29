401c436a4018072f7382d50a1ab3018ee91234b1
Emily Chai <echai93@gmail.com>
2017-03-29 15:41:29 +0100

    DRY up rspec  using web helper

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
+    p params
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
     erb(:play)
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -2,20 +2,14 @@ describe Battle do
 
   feature 'Player details' do
     scenario 'entering names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Emily'
-      fill_in :player_2_name, with: 'Alice'
-      click_button 'Submit'
+      sign_in_and_play
       expect(page).to have_content 'Emily vs. Alice'
     end
   end
 
   feature 'Player hit points' do
     scenario 'view hit points' do
-      visit('/')
-      fill_in :player_1_name, with: 'Emily'
-      fill_in :player_2_name, with: 'Alice'
-      click_button 'Submit'
+      sign_in_and_play
       expect(page).to have_content 'Emily: 10HP'
     end
   end
