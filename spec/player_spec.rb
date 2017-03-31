f12c20014f41900ab24b68017ed308ee2fed4e97
Emily Chai <echai93@gmail.com>
2017-03-31 09:59:33 +0100

    Change controller route to use extracted method in Game class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -16,13 +16,6 @@ describe Player do
       end
     end
 
-    describe '#attack' do
-      it 'damages the player' do
-        expect(player_2).to receive(:receive_damage)
-        player_1.attack(player_2)
-      end
-    end
-
     describe '#receive_damage' do
       it 'reduces the player hp' do
         expect { player_2.receive_damage }.to change { player_2.hp }.by(-2)
