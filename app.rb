b40a19ec133e3419fa98444e755b2ef0165a4a8e
Emily Chai <echai93@gmail.com>
2017-03-31 10:15:53 +0100

    Make controller skinnier

--- app.rb
+++ app.rb
@@ -13,21 +13,22 @@ class Battle < Sinatra::Base
   post '/names' do
     # player names stored as attributes of instances of Player class in global variables
     # require 'pry'; binding.pry
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+    @player_1 = Player.new(params[:player_1])
+    @player_2 = Player.new(params[:player_2])
+    $game = Game.new(@player_1, @player_2)
     redirect to('/play')
   end
 
   get '/play' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
+    @player_1 = $game.player_1.name
+    @player_2 = $game.player_2.name
     erb(:play)
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @player_1 = $game.player_1
+    @player_2 = $game.player_2
+    $game.attack(@player_2)
     erb(:attack)
   end
 
